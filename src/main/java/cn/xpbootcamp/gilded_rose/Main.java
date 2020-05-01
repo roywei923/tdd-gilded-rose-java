3d3144d67fba39db87725447d7bd3f1bf00bb809
puwei <roy.wei@thoughtworks.com>
2020-05-01 20:29:54 +0800

    Create models, main, and MerchandiseTest

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Main.java
@@ -0,0 +1,9 @@
+package cn.xpbootcamp.gilded_rose;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/main/java/cn/xpbootcamp/gilded_rose/model/Merchandise.java
@@ -0,0 +1,15 @@
+package cn.xpbootcamp.gilded_rose.model;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Merchandise {
+
+  private String name;
+  private int sellIn;
+  private int quality;
+
+}
